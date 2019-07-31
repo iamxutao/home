26737acc1a0535723b2c2711a9f65bb9311f86f7
[xutao] <[1104320285@qq.com]>
2019-07-31 15:15:56 +0800

    7.31.3

--- 饥人谷作业/bookMarks/js/myBookmarks.js
+++ 饥人谷作业/bookMarks/js/myBookmarks.js
@@ -1,50 +1,29 @@
-< !DOCTYPE html >
-    <
-    html lang = "en" >
+// 1. 初始化数据
+var hashA = init()
+var keys = hashA['keys']
+var hash = hashA['hash']
 
-    <
-    head >
-    <
-    meta charset = "UTF-8" >
-    <
-    meta name = "viewport"
-content = "width=device-width, initial-scale=1.0" >
-    <!--    <meta name="viewport" content="width=device-width, ,user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">-->
-    <!--	移动端所需-->
-    <
-    meta http - equiv = "X-UA-Compatible"
-content = "ie=edge" >
-    <
-    title > 首页 < /title> <
-    link rel = "stylesheet"
-href = "../css/style.css" >
-    <
-    link rel = "stylesheet"
-href = "../css/myBookmarks.css" >
-    <
-    /head>
+// 2. 生成键盘
+// 遍历 keys，生成 kbd 标签
+generateKeyboard(keys, hash)
 
-<
-body >
-    <
-    header > < /header> <
-    main >
-    <
-    div class = "wrapper"
-id = "main" >
+// 3. 监听用户动作
+listenToUser(hash)
 
-    <
-    /div>
+// 下面是工具函数
+function getFromLocalStorage(name) {
+    return JSON.parse(localStorage.getItem(name) || 'null')
+}
 
-<
-/main> <
-footer > < /footer> <
-    /body> <
-    script src = "../js/jquery-3.3.1.min.js" > < /script> <
-    script src = "../js/myBookmarks.js" > < /script>
+function tag(tagName) {
+    return document.createElement(tagName)
+}
 
-<
-/html>urn span
+function createSpan(textContent) {
+    var span = tag('span')
+    span.textContent = textContent
+    span.className = "text"
+    return span
 }
 
 function createButton(id) {
@@ -82,41 +61,9 @@ function createImage(domain) {
 
 function init() {
     var keys = {
-        '0': {
-            0: 'q',
-            1: 'w',
-            2: 'e',
-            3: 'r',
-            4: 't',
-            5: 'y',
-            6: 'u',
-            7: 'i',
-            8: 'o',
-            9: 'p',
-            length: 10
-        },
-        '1': {
-            0: 'a',
-            1: 's',
-            2: 'd',
-            3: 'f',
-            4: 'g',
-            5: 'h',
-            6: 'j',
-            7: 'k',
-            8: 'l',
-            length: 9
-        },
-        '2': {
-            0: 'z',
-            1: 'x',
-            2: 'c',
-            3: 'v',
-            4: 'b',
-            5: 'n',
-            6: 'm',
-            length: 7
-        },
+        '0': { 0: 'q', 1: 'w', 2: 'e', 3: 'r', 4: 't', 5: 'y', 6: 'u', 7: 'i', 8: 'o', 9: 'p', length: 10 },
+        '1': { 0: 'a', 1: 's', 2: 'd', 3: 'f', 4: 'g', 5: 'h', 6: 'j', 7: 'k', 8: 'l', length: 9 },
+        '2': { 0: 'z', 1: 'x', 2: 'c', 3: 'v', 4: 'b', 5: 'n', 6: 'm', length: 7 },
         'length': 3
     }
     var hash = {
@@ -150,10 +97,13 @@ function generateKeyboard(keys, hash) {
     for (var index = 0; index < keys['length']; index = index + 1) {
         var div = tag('div')
         div.className = 'row'
+
         main.appendChild(div)
+
         var row = keys[index] // 第一个数组  第二个数组  第三个数组
         for (var index2 = 0; index2 < row['length']; index2 = index2 + 1) {
             var span = createSpan(row[index2])
+
             var button = createButton(row[index2])
 
             var img = createImage(hash[row[index2]])
